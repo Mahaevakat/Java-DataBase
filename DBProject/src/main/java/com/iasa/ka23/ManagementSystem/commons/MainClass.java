271d43e9df0082d81af88945e147f0c8bf65a46f
petiteandreas <andrii.d.nikitin@gmail.com>
2015-05-23 00:26:10 +0300

    added admin and operator buttons on mainframe

--- DBProject/src/main/java/com/iasa/ka23/ManagementSystem/commons/MainClass.java
+++ DBProject/src/main/java/com/iasa/ka23/ManagementSystem/commons/MainClass.java
@@ -1,12 +1,12 @@
 package com.iasa.ka23.ManagementSystem.commons;
 
-import com.iasa.ka23.ManagementSystem.ui.MainJFrame;
+import static com.iasa.ka23.ManagementSystem.ui.UIManager.*;
 
 public class MainClass {
 	
 	 public static void main(final String[] args) {
 		SpringContextLoader.loadContext();
-		new MainJFrame().setVisible(true);
+		openWelcomeFrame();
 	}
 }
 
